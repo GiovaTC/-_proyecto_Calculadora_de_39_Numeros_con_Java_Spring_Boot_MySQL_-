ae50b1a9ed729ddfafc13e8b8bd9e983799f9d68
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-06-26 12:39:27 -0500

    commit - NumeroService .

--- src/main/java/com/example/Calculadora39Numeros/servicio/NumeroService.java
+++ src/main/java/com/example/Calculadora39Numeros/servicio/NumeroService.java
@@ -1,4 +1,32 @@
 package com.example.Calculadora39Numeros.servicio;
 
+import com.example.Calculadora39Numeros.entidad.Numero;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.Calculadora39Numeros.repositorio.NumeroRepository;
+
+import java.util.List;
+
+@Service
 public class NumeroService {
-}
+
+    @Autowired
+    private NumeroRepository repository;
+
+    public void guardar(List<Numero> lista) {
+
+        repository.deleteAll();
+
+        repository.saveAll(lista);
+
+    }
+
+    public Double sumar() {
+
+        return repository.findAll()
+                .stream()
+                .mapToDouble(Numero::getValor)
+                .sum();
+
+    }
+}   
