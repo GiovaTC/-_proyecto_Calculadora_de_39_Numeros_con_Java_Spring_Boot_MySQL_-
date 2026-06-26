f63850c29f1917a05406178b06a2b901e59453fd
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-06-26 12:35:19 -0500

    commit - Numero / .

--- src/main/java/com/example/Calculadora39Numeros/entidad/Numero.java
+++ src/main/java/com/example/Calculadora39Numeros/entidad/Numero.java
@@ -1,4 +1,33 @@
 package com.example.Calculadora39Numeros.entidad;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "numeros")
 public class Numero {
-}
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Double valor;
+
+    public Numero() {
+
+    }
+
+    public Numero(Double valor) {
+        this.valor = valor;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}   
